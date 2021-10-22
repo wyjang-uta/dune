8bf62976639b29d6984dd00c5b5c60aa960e0dbb
Wooyoung Jang <wyjang@master.tier3-atlas.uta.edu>
2021-10-22 13:03:51 -0500

    build directory is deleted.

--- sim/g4/TargetSim_Nu/build/CMakeFiles/DUNE-TargetSim.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/DUNE-TargetSim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DUNE-TargetSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
